e371a967cc9440993ce5fb65e55c36038fe3be50
Benny Suryajaya <benny@imrc.ncku.edu.tw>
2022-06-07 17:29:35 +0800

    Added Kafka plugin to Unity script

--- Test.cs
+++ Test.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//added for Kafka
+using Confluent.Kafka;
+using System.Threading;
+
 public class OP2MoveDoor : MonoBehaviour
 {
     //int x_axis = 0;
@@ -12,6 +16,13 @@ public class OP2MoveDoor : MonoBehaviour
     //float speed = 50f;
     int position_way = 0; //0不動，1開門，2關門
     float movingSpeed = 500f;
+
+    //Parameters for Kafka thread
+    bool kafkaStarted = false;
+    Thread kafkaThread;
+    threadHandle _handle;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +30,9 @@ public class OP2MoveDoor : MonoBehaviour
         cur_y = this.transform.position.y;
         cur_z = this.transform.position.z;
         position_way = 1;
+
+        //Start the Kafka thread
+        StartKafkaThread();
     }
 
     // Update is called once per frame
@@ -59,5 +73,125 @@ public class OP2MoveDoor : MonoBehaviour
         
         //transform
         //tra
+
+        //Check if Kafka is stopped using Ctrl+C
+        if (Input.GetKeyUp(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.C))
+        {
+            Debug.Log("Cancelling Kafka!");
+            StopKafkaThread();
+        }
+        //Process incoming messages from Kafka stream
+        ProcessKafkaMessage();
+    }
+
+
+    //class for handling Kafka thread
+    public class threadHandle
+    {
+        ConsumerConfig config;
+        Message<Ignore, string> message = null;
+        // public readonly ConcurrentQueue<StreamMessage> _queue = new ConcurrentQueue<StreamMessage>();
+
+
+        public void StartKafkaListener()
+        {
+            Debug.Log("Kafka - Starting Thread..");
+            try
+            {
+                config = new ConsumerConfig
+                {
+                    GroupID = "",
+                    BootstrapServers = "",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+
+                Debug.Log("Kafka - Created config");
+
+                using (var c = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    c.Subscribe("networktopicdata");
+                    Debug.Log("Kafka - Subscribed");
+
+                    CancellationTokenSource cts = new CancellationTokenSource();
+                    Console.CancelKeyPress += (_, e) => {
+                        e.Cancel = true; //prevent the process from terminating
+                        cts.Cancel();
+                    };
+
+                    try
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                //Waiting for message
+                                var cr = c.Consume(cts.Token);
+                                message = cr.Message;
+                                Debug.Log(message.Value);
+                                //Got message! Decode and put on queue
+                                //StreamMessage message = ParseStreamMessage.Decode(cr.Value);
+                                //_queue.Enqueue(message);
+                                try
+                                {
+                                    c.Commit(cr);
+                                }
+                                catch (KafkaException e)
+                                {
+                                    Debug.Log("Kafka - Error occured: " + e.Error.Reason);
+                                }
+                            }
+                            catch (ConsumeException e)
+                            {
+                                Debug.Log("Consume - Error occured: " + e.Error.Reason);
+                            }
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        Debug.Log("Kafka - Canceled..");
+                        // Ensure the consumer leaves the group cleanly and final offset are committed. 
+                        c.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Kafka - Received Exception: " + ex.Message + " trace: " + ex.StackTrace);
+            }
+        }
+    }
+
+    public void StartKafkaThread()
+    {
+        if  (kafkaStarted) return;
+
+        _handle = new threadHandle();
+        kafkaThread = new Thread(_handle.StartKafkaListener);
+
+        kafkaThread.Start();
+        kafkaStarted = true;
+        //StartKafkaListener(config);
+    }
+
+    private void ProcessKafkaMessage()
+    {
+        if (kafkaStarted)
+        {
+            // StreamMessage message;
+            // while (_handle._queue.TryDequeue(out message))
+            // {
+            //     ProcessPBMessages.Process(message);
+            // }
+        }
+    }
+
+    void StopKafkaThread()
+    {
+        if (kafkaStarted)
+        {
+            kafkaThread.Abort();
+            kafkaThread.Join();
+            kafkaStarted = false;
+        }
     }
 }
